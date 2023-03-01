79d5fd80f360d7f12e9f7c92c30adb5d562cda8b
zkjet <zkjet@mailfence.com>
2023-02-28 22:49:34 -0700

    added metamask wallet connection support

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,46 @@
 // pages/_app.js
 
 // 1. Import `createTheme`
-import { createTheme, NextUIProvider } from "@nextui-org/react"
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import { ConnectWallet } from "@thirdweb-dev/react";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { Container, Card, Row, Text } from "@nextui-org/react";
+
+const activeChainId = ChainId.Goerli;
 
 // 2. Call `createTheme` and pass your custom theme values
 const theme = createTheme({
   type: "dark", // it could be "light" or "dark"
   theme: {
     colors: {
-      primary: '#4ADE7B',
-      secondary: '#F9CB80',
-      error: '#FCC5D8',
+      primary: "#4ADE7B",
+      secondary: "#F9CB80",
+      error: "#FCC5D8",
     },
-  }
-})
+  },
+});
 
 // 3. Pass the new `theme`` to the `NextUIProvider`
 function MyApp({ Component, pageProps }) {
   return (
     <NextUIProvider theme={theme}>
-      <Component {...pageProps} />
-      <div>Connect To Wallet</div>
+      <ThirdwebProvider network={activeChainId}>
+        <Component {...pageProps} />
+        <Container>
+          <Card css={{ $$cardColor: "$colors$primary" }}>
+            <Card.Body>
+              <Row justify="center" align="center">
+                <Text h6 size={15} color="black" css={{ m: 0 }}>
+                  Welcome to Resume3.xyz
+                </Text>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Container>
+        <ConnectWallet accentColor="#f213a4" colorMode="dark" />
+      </ThirdwebProvider>
     </NextUIProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
