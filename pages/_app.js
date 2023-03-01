b5cb3b1aa44ea4c198b7d8ade0fe7a25e5f41fea
zkjet <zkjet@mailfence.com>
2023-02-28 23:24:18 -0700

    UI draft prototype

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,10 @@
 import { createTheme, NextUIProvider } from "@nextui-org/react";
 import { ConnectWallet } from "@thirdweb-dev/react";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import { Container, Card, Row, Text } from "@nextui-org/react";
+import { Container, Card, Row, Text, Avatar } from "@nextui-org/react";
+import { Input, Grid } from "@nextui-org/react";
+import { Checkbox, Button } from "@nextui-org/react";
+import { Spacer } from "@nextui-org/react";
 
 const activeChainId = ChainId.Goerli;
 
@@ -26,7 +29,7 @@ function MyApp({ Component, pageProps }) {
     <NextUIProvider theme={theme}>
       <ThirdwebProvider network={activeChainId}>
         <Component {...pageProps} />
-        <Container>
+        <Container fluid>
           <Card css={{ $$cardColor: "$colors$primary" }}>
             <Card.Body>
               <Row justify="center" align="center">
@@ -36,8 +39,87 @@ function MyApp({ Component, pageProps }) {
               </Row>
             </Card.Body>
           </Card>
+          <Spacer y={2} />
         </Container>
-        <ConnectWallet accentColor="#f213a4" colorMode="dark" />
+        <Row justify="left" align="left">
+          <Avatar
+            squared
+            src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+          />{" "}
+          <ConnectWallet accentColor="#f213a4" colorMode="dark" />{" "}
+          <h3>EMPLOYERS | APPLICANTS | ABOUT US | CONTACT US | HOME</h3>
+        </Row>
+        <Spacer y={1} />
+
+        <Grid.Container gap={4}>
+          <Grid>
+            <Input bordered labelPlaceholder="Email" color="default" />
+          </Grid>
+          <Grid>
+            <Input
+              bordered
+              labelPlaceholder="Years Experience"
+              color="primary"
+            />
+          </Grid>
+          <Grid>
+            <Input bordered labelPlaceholder="Phone" color="secondary" />
+          </Grid>
+          <Grid>
+            <Input bordered labelPlaceholder="Location" color="success" />
+          </Grid>
+        </Grid.Container>
+        <Spacer y={1} />
+
+        <Checkbox.Group
+          label="Select Chains"
+          orientation="horizontal"
+          color="secondary"
+          defaultValue={["ethereum"]}
+        >
+          <Checkbox value="ethereum">Ethereum</Checkbox>
+          <Checkbox value="avalanche">Avalanche</Checkbox>
+          <Checkbox value="binance">Binance Smart Chain</Checkbox>
+          <Checkbox value="solana">Solana</Checkbox>
+        </Checkbox.Group>
+        <Spacer y={1} />
+
+        <Checkbox.Group
+          label="Select Technologies"
+          orientation="horizontal"
+          color="secondary"
+          defaultValue={["javascript", "typescript"]}
+        >
+          <Checkbox value="javascript">JavaScript</Checkbox>
+          <Checkbox value="typescript">TypeScript</Checkbox>
+          <Checkbox value="node">Node.js</Checkbox>
+          <Checkbox value="react">React.js</Checkbox>
+        </Checkbox.Group>
+        <Spacer y={1} />
+
+        <Checkbox.Group
+          label="Select Smart Contract Languages"
+          orientation="horizontal"
+          color="secondary"
+          defaultValue={["Solidity"]}
+        >
+          <Checkbox value="solidity">Solidity</Checkbox>
+          <Checkbox value="vyper">Vyper</Checkbox>
+          <Checkbox value="yul">YUL</Checkbox>
+          <Checkbox value="rust">Rust</Checkbox>
+        </Checkbox.Group>
+        <Spacer y={1} />
+        <Row>
+          <Button>WRITE CONTRACT</Button>
+          <Input
+            placeholder="Employers Address 0x0000000000000000000000000000000000000000"
+            width="500px"
+          />
+        </Row>
+        <Text>
+          Your submission will write to the blockchain, and notify the wallet of
+          your submission. Standby for your TX Hash for confirmation.
+        </Text>
       </ThirdwebProvider>
     </NextUIProvider>
   );
